3e295ede2eff4f52a956d15ba856d06a97489784
mariari <mariari@protonmail.com>
2022-03-21 00:17:48 -0400

    Setup the structure of the defpackage for the linearization

--- src/pass/package.lisp
+++ src/pass/package.lisp
@@ -1,15 +1,16 @@
-(defpackage #:alu.pass.linear-term
+;; we are use define-package just for reexport. Note that we can't
+;; make package nicknames on older versions of asdf
+(uiop:define-package #:alu.pass.linear-term
   (:documentation "Provides a simplified term structure that has been
 through linearization")
-  (:use #:common-lisp #:serapeum)
-  (:local-nicknames (:util :alu.utils)
-                    (:spc  :alu.spec))
+  (:use #:common-lisp #:serapeum #:alu.spec)
+  (:reexport :alu.spec)
   (:export))
 
 (defpackage #:alu.pass
   (:documentation "Provides simplification passes to the Alucard Language")
   (:use #:common-lisp #:serapeum)
   (:local-nicknames (:util    :alu.utils)
-                    (:spc     :alu.spec)
+                    (:spc     :alu.pass.linear-term)
                     (:storage :alu.storage))
   (:export))
