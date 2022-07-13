0bd9a1ef7cd6496ac67f4c17ae43af96a39b4885
mariari <mariari@protonmail.com>
2022-07-13 22:56:10 +0800

    Add splitting declarations properly along with adding sections to defun

--- src/stepper/define.lisp
+++ src/stepper/define.lisp
@@ -1,4 +1,8 @@
 (in-package #:alu.stepper.define)
 
 (defmacro defun (name lambda-list &rest body &environment env)
-  `(cl:defun ,name ,lambda-list ,@(step:body body env)))
+  (destructuring-bind (decs body) (step:split-declaration body)
+    `(cl:defun ,name ,lambda-list
+       ,@decs
+       (stack:with-section ,name
+         ,@(step:body body env)))))
