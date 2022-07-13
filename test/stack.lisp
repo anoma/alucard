4b14e17dca082e90a620fbe907803cce76f2034d
mariari <mariari@protonmail.com>
2022-07-13 22:24:07 +0800

    Add more complicated tests that test the printer and edge cases of the stack

--- test/stack.lisp
+++ test/stack.lisp
@@ -7,7 +7,7 @@
 
 ;; I could be much more thorough in testing this, but it would just
 ;; involve me mimicking calls tediously.
- 
+
 (test dynamic-variable-respected
   (let ((current-stack (stack:get)))
     (stack:with-empty-stack ()
@@ -32,3 +32,70 @@
          '((+ e f g)
            (+ b c d)
            (+ 1 2 3))))))
+
+;; I'm being lazy I shouldn't check the printed version, but rather
+;; make equality or a quick way to check equality of my objects
+
+(defparameter *inner-output*
+"((:IN BAZ
+  (/ C D)
+  (* A B (/ C D)))
+ (:IN FAZ
+  (/ C D)
+  (* A B (/ C D))))")
+
+(defparameter *outer-output*
+"((:IN FAZ
+  (/ C D)
+  (* A B (/ C D))))")
+
+(defparameter *outer-cdr*
+"((:IN FAZ
+  (* A B (/ C D))))")
+
+
+;; Being lazy about testing and using streams.
+(test removing-works-as-expected
+  (let ((*print-pretty* t)
+        (output-stream-baz        (make-string-output-stream))
+        (output-stream-faz        (make-string-output-stream))
+        (output-stream-manual     (make-string-output-stream))
+        (output-stream-manual-cdr (make-string-output-stream)))
+    (stack:with-empty-stack ()
+      (stack:with-section faz
+        (stack:push '(* a b (/ c d)))
+        (stack:push '(/ c d))
+        (stack:with-section baz
+          (stack:push '(* a b (/ c d)))
+          (stack:push '(/ c d))
+          ;; dump the current stack to the output stream
+          (print-object (stack:get) output-stream-baz)
+          ;; we cdr 3 times to pop the baz out of it!, thus we should
+          ;; be the same as faz. and the printing should also work
+          ;; exactly the same.
+          (let ((remove-baz-by-hand
+                  (stack:cdr (stack:cdr
+                              (stack:cdr (stack:get))))))
+            (print-object remove-baz-by-hand output-stream-manual)
+            ;; this should cdr off an element off faz
+            (print-object (stack:cdr remove-baz-by-hand)
+                          output-stream-manual-cdr)))
+        ;; weird how the formatting changes between calls
+        (print-object (stack:get) output-stream-faz)
+        (let ((output-faz (get-output-stream-string output-stream-faz)))
+          (is (string= *inner-output*
+                       (get-output-stream-string output-stream-baz))
+              "Scope should be respected and we should be double
+              nested")
+          (is (string= output-faz
+                       *outer-output*)
+              "formatting for this should be precise and nice, with
+              the outer section gone outside of the scope")
+          (is (string= (get-output-stream-string output-stream-manual)
+                       output-faz)
+              "Removing the two elements in baz plus the baz function
+              marker should be the same as leaving the section")
+          (is (string= (get-output-stream-string output-stream-manual-cdr)
+                       *outer-cdr*)
+              "Cdring a nil section should lead to cdring the next
+              section on the stack"))))))
