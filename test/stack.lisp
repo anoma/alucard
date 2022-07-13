dcd31c55f7971369788cb9f0d464c4a8f39c037d
mariari <mariari@protonmail.com>
2022-07-13 19:16:07 +0800

    Fixup the tests that were broken

--- test/stack.lisp
+++ test/stack.lisp
@@ -5,6 +5,9 @@
 
 (in-suite alucard.stack)
 
+;; I could be much more thorough in testing this, but it would just
+;; involve me mimicking calls tediously.
+ 
 (test dynamic-variable-respected
   (let ((current-stack (stack:get)))
     (stack:with-empty-stack ()
@@ -13,8 +16,19 @@
       (stack:push 20)
       (is (equalp
            (list 20 10 3)
-           (stack:get))
+           (stack:stack (stack:get)))
           "We should be pushing on a fresh stack"))
     (is (equalp (stack:get)
                 current-stack)
         "Our operations should not have pushed globally")))
+
+(test function-section
+  (stack:with-section faz
+    (stack:push '(+ 1 2 3))
+    (stack:push '(+ b c d))
+    (stack:push '(+ e f g))
+    (is (equalp
+         (stack:stack (stack:current-section (stack:get)))
+         '((+ e f g)
+           (+ b c d)
+           (+ 1 2 3))))))
