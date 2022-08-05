e87313adfe90d612ed5658c31b902c441ed0c0e8
mariari <mariari@protonmail.com>
2022-08-05 17:54:55 +0800

    Add a make file and quit function to aluser

---
+++ Makefile
@@ -0,0 +1,9 @@
+all:
+	 ros run --load "alu.asd" --eval "(progn (ql:quickload :alu) (asdf:make :alu))"
+
+install:
+	ros run --load "alu.asd" --eval "(progn (ql:quickload :alu) (asdf:make :alu))"
+	mv "./build/alu.image" '${HOME}/.local/bin/'
+
+clean:
+	rm "./build/alu.image"
--- alu/package.lisp
+++ alu/package.lisp
@@ -9,13 +9,14 @@
                     (:pipeline :alu.pipeline)
                     (:pass     :alu.pass)
                     (:def      :alu.stepper.define))
-  (:mix #:alu #:common-lisp)
+  (:mix #:alu #:common-lisp #:uiop)
   (:export #:+ #:* #:= #:exp #:deftype #:defcircuit #:def
            #:deflex #:bool #:entry-point #:coerce
            #:check #:get #:array #:to-array
            #:with-constraint
            #:vampir
-           #:vampir-keyword))
+           #:vampir-keyword
+           #:quit))
 
 ;; we should probably move this to the very end of the load order, so
 ;; we can make it a very nice package for users to use.
